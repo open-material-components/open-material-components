4a13c83dba664b8f18b130f2a9b9f386ec40493d
Lars den Bakker <larsdenbakker@gmail.com>
2019-05-05 18:45:51 +0200

    feat(button): implement material button

--- packages/button/stories/index.stories.js
+++ packages/button/stories/index.stories.js
@@ -2,9 +2,28 @@ import { storiesOf, html } from '@open-wc/demoing-storybook';
 
 import '../omc-button.js';
 
-storiesOf('OmcButton|Material Extras', module).add(
-  'Overview',
-  () => html`
-    <omc-button></omc-button>
-  `,
-);
+storiesOf('OmcButton|Material Extras', module)
+  .add(
+    'Text button',
+    () => html`
+      <omc-button>Click here</omc-button>
+      <omc-button>Show</omc-button>
+      <omc-button disabled>Disabled</omc-button>
+    `,
+  )
+  .add(
+    'Outlined button',
+    () => html`
+      <omc-button outlined>Click here</omc-button>
+      <omc-button outlined>Show</omc-button>
+      <omc-button outlined disabled>Disabled</omc-button>
+    `,
+  )
+  .add(
+    'Contained button',
+    () => html`
+      <omc-button contained>Click here</omc-button>
+      <omc-button contained>Show</omc-button>
+      <omc-button contained disabled>Disabled</omc-button>
+    `,
+  );
