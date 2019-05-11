b1acde91f2f045458c8d755c0c5001226a940e08
Thomas Allmer <d4kmor@gmail.com>
2019-05-11 16:35:48 +0200

    chore: load all stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,9 @@
 import { configure } from '@storybook/polymer';
 import '@storybook/addon-console';
 
-// const req = require.context("../stories", true, /\.stories\.js$/);
-// function loadStories() {
-//   req.keys().forEach(filename => req(filename));
-// }
+const req = require.context('../stories', true, /\.stories\.js$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
 
-configure(() => {
-  return require('../packages/button/stories/index.stories.js');
-}, module);
+configure(loadStories, module);
